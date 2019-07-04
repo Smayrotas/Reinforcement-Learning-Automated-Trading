591be2c8bcfabec97155a31643cb318d1d3904cd
Stelios Mavrotas <mavrotas.st@gmail.com>
2019-07-04 20:47:37 +0100

    Update environment.py

--- environment.py
+++ environment.py
@@ -10,7 +10,6 @@ from matplotlib import pyplot as plt
 #Check Out what gym.Env should be returning
 
 
-
 '''FOCUS ON ONLY ONE TRADE DECISIONS'''
 
 '''
@@ -23,50 +22,57 @@ Primary Rules for trading:
 --> All these parameters can be changed later when I have more experience in machine learning
 '''
 
+'''
+-->Things to do over the next few days
+1. Update the capital with profit/los
+
+2. Work on Optimising the Reward Function
+
+'''
+
 class tradingEnvironment(gym.Env):
 
-    #The __init__ method is only called once in the firstRun with trainData and initial to Invest
+
     def __init__(self,trainData,cashInHand,uPnL):
 
-        self.n_Assets=1 #This has to be the number of currencies I am trading with
+        self.n_Assets=1
 
-        '''Generally might not need to use environments but check when you run it'''
 
-        self.fig = plt.figure()
-        self.x = list()
-        self.y = list()
-        self.r=list()
-        fig, self.ax1 = plt.subplots()
-        self.ax2 = self.ax1.twinx()
 
         env = gym.make('Copy-v0')
         env.reset()
 
+        self.y = list()
+        self.x = list()
+
         self.trainData=trainData
         self.currentStep=0
-        self.maxStep=100
+        self.maxStep=50000
 
         self.currency_Price=None
 
-
-        #self.step(actionsReceived.action)
 
         self.initValue=cashInHand
         self.initPnL=uPnL
 
         self.unrealizedPnl=self.initPnL
 
-        self.tradesInfo=np.zeros((10**5,5)) #Array with ID|Ticker Symbol|BOUGHT PRICE|SOLD PRICE|VOLUME in the future this can have quantity as well collumns
+        self.tradesInfo=np.zeros((self.maxStep*2,5)) #Array with ID|Ticker Symbol|BOUGHT PRICE|SOLD PRICE|VOLUME in the future this can have quantity as well collumns
 
         '''
         Actions: Buy, Sell, Hold
+        
+        Real Action Environment:3*noOfStocksOwned
+        
+        --> Maybe think about the states more
+        
         States: Long, Neutral
+        
+        Real States Could be --> [Number of Stocks Owned, Stock Prices, Cash in Hand]
         '''
 
         self.openPositions=0
 
-
-        '''------------Fix the Code Below----------'''
 
         states=['Neutral','Long']
         actions=['Buy','Sell','Hold']
@@ -78,24 +84,18 @@ class tradingEnvironment(gym.Env):
 
 
     def _step(self, action):
-        '''Fix the reward and the updateInfotable methods'''
+
         self.updatePositions(action)
         self.currency_Price=self.trainData[self.currentStep]
-
-        self.y.append(self.currency_Price)
-        self.x.append(self.currentStep)
-        self.ax1.plot(self.x, self.y,'-g')
-
-
 
 
         observation=self.observeCurrentState()
         self.updateInfoTable(action)
         reward=self.updatePnl()
 
-        self.r.append(reward)
-        self.ax2.plot(self.x,self.r,'r')
-        plt.draw()
+
+        self.x.append(self.currentStep)
+        self.y.append(reward)
 
         info={'unrealized PnL':self.unrealizedPnl}
         if self.currentStep==self.maxStep:
@@ -109,8 +109,9 @@ class tradingEnvironment(gym.Env):
         return observation,reward,done,info
 
     def _reset(self):
-        '''------When you reset you need to reset all the other variables such as initial pnl etc------'''
         self.openPositions=0
+        self.x=list()
+        self.y = list()
         self.currentStep=0
         self.updateInfoTable('Delete')
         return self.observeCurrentState()
@@ -125,25 +126,29 @@ class tradingEnvironment(gym.Env):
     def updateInfoTable(self,action):
 
         if action=='Delete':
-            self.tradesInfo=np.zeros((10**5,5))
+            self.tradesInfo=np.zeros((self.maxStep*2,5))
         else:
+            self.tradesInfo[self.currentStep, 0] = self.currentStep + 1
+
             if action==2:
                 return
             elif action==0:
                 self.tradesInfo[self.currentStep, 2] = self.currency_Price
             elif action==1:
                 self.tradesInfo[self.currentStep, 3] = self.currency_Price
-
-            self.tradesInfo[self.currentStep, 0] = self.currentStep + 1
-            self.tradesInfo[self.currentStep, 4] = 100
+            self.tradesInfo[self.currentStep, 4] = 100000
             '''Lets assume volume is constant at 100 for now'''
         return
 
     def updatePositions(self,action):
+
+        try:
             if (action==0):
                 self.openPositions+=1
             elif (action==1):
                 self.openPositions+=-1
+        except:
+            print('a')
 
     def rememberAgent(self,agent):
         self.agent=agent
@@ -152,21 +157,28 @@ class tradingEnvironment(gym.Env):
     def updatePnl(self):
 
         ids=self.tradesInfo[:,0]
-        a=len(np.nonzero(ids))-1
+        a=len(np.nonzero(ids)[0])-1
         Volume=self.tradesInfo[a,4]
 
-        if self.openPositions==0:
-            return 0
-        elif self.openPositions==1:
-            BoughtPrice = self.tradesInfo[a, 2]
-            SoldPrice = self.tradesInfo[a-1, 3]
-            updatedPnl=Volume*(SoldPrice-BoughtPrice)
+        PreviousBoughPrice=self.tradesInfo[a-1,2]
+        CurrentSellPrice=self.tradesInfo[a,3]
+
+        if  (PreviousBoughPrice is not 0) and (CurrentSellPrice is not 0) and (a is not 0):
+            updatedPnl = Volume * (CurrentSellPrice- PreviousBoughPrice)
             return updatedPnl
+
+        elif self.tradesInfo[a-1,3] is not 0:
+           return 0
 
 
     def observeCurrentState(self):
         '''States  Neutral(0) or Long(1) thus return ALL actions for that state'''
         Q=self.agent.Q
-        return Q[:,self.openPositions], self.openPositions
+
+        try:
+            return Q[:,self.openPositions], self.openPositions
+        except:
+            return 0
+
 
 
